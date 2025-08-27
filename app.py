ba905d27e58e978474df4f5e266ced9f266b8871
Joe Mulberry <joemulberry0@gmail.com>
2025-08-27 20:16:00 +0100

    Update app with latest changes

--- app.py
+++ app.py
@@ -88,8 +88,8 @@ def parsePlayer(url):
     leagueInfo = bigBox.find('span', class_='data-header__league')
 
     store['currentLeague'] = leagueInfo.find('img')['alt']
-    store['currentLeagueURL'] = bigBox.find('a')['href']
-    store['currentLeagueID'] = int(store['currentClubURL'].split('/')[-1])
+    store['currentLeagueURL'] = leagueInfo.find('a')['href']
+    store['currentLeagueID'] = int(store['currentLeagueURL'].split('/')[-1])
     
 
     store['leagueInfo'] = leagueInfo
