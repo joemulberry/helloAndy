772bc9b1fc691e80cd10561d0ccf9560db81d67d
Joe Mulberry <joemulberry0@gmail.com>
2025-08-27 19:51:55 +0100

    Update app with latest changes

--- app.py
+++ app.py
@@ -6,12 +6,28 @@ from random import choice
 import requests 
 
 
+# ------- undected scrap for Transfermarkt -------- #
+def get_souped_page(page_url):
+        headers_list = [
+            {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'},
+            {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15'},
+            {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+        ]
+        headers = choice(headers_list)
+        pageTree = requests.get(page_url, headers=headers)
+        pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+        return(pageSoup)
+
 
 
 def parsePlayer(url):
-    pass
 
+    # ------- playground -------- #
 
+    url = 'https://www.transfermarkt.co.uk/jude-bellingham/profil/spieler/581678'
+    soup = get_souped_page(url)
+
+    st.write(soup)
 
 
 
@@ -39,6 +55,7 @@ if st.session_state.authenticated:
 
     if playerURL != "":
         st.write(playerURL)
+        parsePlayer(playerURL)
     
 
 if __name__ == "__main__":
