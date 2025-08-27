ab1b083750e8b61419a006c1d7173227118cefbc
Joe Mulberry <joemulberry0@gmail.com>
2025-08-27 19:27:24 +0100

    Update app with latest changes

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import streamlit as st
 from datetime import datetime
 
+st.set_page_config(page_title="Hello Streamlit", page_icon="👋", layout="centered")
+
 APP_PASSWORD = "letmein"
 
 if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 
 if not st.session_state.authenticated:
-    st.title('GBE/ESC Checker')
+    st.title('GBE/ESC Checkerk')
     password = st.text_input("Enter password", type="password")
     if password:
         if password == APP_PASSWORD:
             st.session_state.authenticated = True
+            st.experimental_rerun()
         else:
             st.error("Wrong password, try again.")
 
 if st.session_state.authenticated:
-    st.set_page_config(page_title="Hello Streamlit", page_icon="👋", layout="centered")
+    st.title('GBE/ESC Checker')
 
     st.title("Hello, Streamlit 👋")
     st.write("This is a bare-bones app. Use the sidebar to interact.")
