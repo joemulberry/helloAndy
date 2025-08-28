c3a53b199479eba6e6280351285fa8a875ef8c8f
Joe Mulberry <joemulberry0@gmail.com>
2025-08-28 17:28:35 +0100

    Update app with latest changes

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def getNationalTeam(playerURL, transferDate):
         part2 = '&ende=' + date.today().strftime("%d.%m.%Y") + '&nurEinsatz=0'
         nationalTeamURLSubSection =  nationalTeamURL + part1 + startDate + part2
         soup2 = get_souped_page(nationalTeamURLSubSection)
-
+        print(nationalTeamURLSubSection)
         if len(soup2.find_all('table')) >= 3:
             table = soup2.find_all('table')[3]
             tbody = table.find('tbody')
