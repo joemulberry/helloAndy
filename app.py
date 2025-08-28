654370b3323d2f5977ce345cf0abb2168ed40a9a
Joe Mulberry <joemulberry0@gmail.com>
2025-08-28 20:30:56 +0100

    Update app with latest changes

--- app.py
+++ app.py
@@ -306,7 +306,7 @@ if st.session_state.authenticated:
             age_years = None
             dob_str = "Unknown"
 
-        st.markdown("##### " + playerInfo.get('name', '') + ' [' + {country} + '] (' + {dob_str} +')')
+        st.markdown("##### " + playerInfo.get('name', '') + ' [' + {country} + '] (' + {age_years if age_years is not None else '—'} +' yrs)')
 
         colA, colB, colC, colD = st.columns([1, 2, 2, 2])
         with colA:
