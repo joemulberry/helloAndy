2b3cc718691d6132b7d77a8e1088a9cdd255cffe
Joe Mulberry <joemulberry0@gmail.com>
2025-08-28 11:02:06 +0100

    Update app with latest changes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def getNationalTeam(playerURL, transferDate, teamID = '20796'):
     nationalTeamURLSubSection =  nationalTeamURL + part1 + startDate + part2
     soup = get_souped_page(nationalTeamURLSubSection)
 
-    table = soup.find_all('table')[2]
+    table = soup.find_all('table')
 
     return table 
         
