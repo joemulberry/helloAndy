6e79a8e9522c567fdeb3067976f241be223f7b08
Joe Mulberry <joemulberry0@gmail.com>
2025-08-29 20:46:42 +0100

    Update app with latest changes

--- app.py
+++ app.py
@@ -9,24 +9,7 @@ from internationalMatches import international_appearance_points
 from fifaPull import fetch_fifa_rankings_timeseries, average_rank
 from utils import * 
 from parse import parsePlayer
-
-
-
-# ------- undected scrap for Transfermarkt -------- #
-def get_souped_page(page_url):
-        headers_list = [
-            {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'},
-            {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15'},
-            {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
-        ]
-        headers = choice(headers_list)
-        pageTree = requests.get(page_url, headers=headers)
-        pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
-        return(pageSoup)
-
-
-
-
+from autoPass import autoPass
 
 
 APP_PASSWORD = "letmein"
@@ -114,22 +97,6 @@ if st.session_state.authenticated:
 
   
     ntInfo = pd.DataFrame(getNationalTeam(playerURL, transferDate))
-    # st.write(ntInfo)
-    # Show per-team metrics and table
-    # for team in sorted(ntInfo['for'].unique()):
-    #     team_df = ntInfo[(ntInfo['for'] == team) & (ntInfo['competitionID'] != 'FS')].copy()
-    #     team_games = len(team_df)
-    #     player_games = int(team_df['played'].sum()) if 'played' in team_df.columns else 0
-    #     pct_played = (player_games / team_games * 100) if team_games else 0.0
-
-    #     # st.subheader(team)
-        # col1, col2, col3 = st.columns(3)
-        # with col1:
-        #     st.metric("Competitive Team matches", team_games)
-        # with col2:
-        #     st.metric("Player played", player_games)
-        # with col3:
-        #     st.metric("% of competitive matches", f"{pct_played:.1f}%")
 
     items = ['U21', 'U19', 'U18']
     unique_teams = list(ntInfo['for'].unique())
@@ -156,49 +123,8 @@ if st.session_state.authenticated:
         #     # dd['internationalPoints'] = international_appearance_points(rank,pct_played)
 
         # st.write(ntInfo['for'][0], "Average rank (last 30 months):", average_rank(x))
-
-        if rank <=10:
-            rankTarget = .30
-            if pct_played >= rankTarget:
-                st.success(f"AUTO-PASS GRANTED: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months for a team ranked between 1-10 [ {rank} ]")
-            elif pct_played >= .20:
-                st.warning(f"AUTO-PASS NEAR-MISS: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is just below the {rankTarget*100}% threshold for a team ranked between 1-10 [ {rank} ] but they are close to meeting the auto-pass threshold")
-            else:
-                st.error(f"AUTO-PASS FAIL: The player has only played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is not above the {rankTarget*100}%  threshold for a team ranked between 1-10 [ {rank} ]")
+        autoPass(rank, pct_played, final_teams)
         
-        elif rank <=20:
-            rankTarget = .40
-            if pct_played >= rankTarget:
-                st.success(f"AUTO-PASS GRANTED: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months for a team ranked between 11-20 [ {rank} ]")
-            elif pct_played >= .20:
-                st.warning(f"AUTO-PASS NEAR-MISS: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is just below the {rankTarget*100}% threshold for a team ranked between 11-20 [ {rank} ] but they are close to meeting the auto-pass threshold")
-            else:
-                st.error(f"AUTO-PASS FAIL: The player has only played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is not above the {rankTarget*100}%  threshold for a team ranked between 11-20 [ {rank} ]")
-        
-        elif rank <=30:
-            rankTarget = .60
-            if pct_played >= rankTarget:
-                st.success(f"AUTO-PASS GRANTED: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months for a team ranked between 21–30 [ {rank} ]")
-            elif pct_played >= .20:
-                st.warning(f"AUTO-PASS NEAR-MISS: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is just below the {rankTarget*100}% threshold for a team ranked between 21–30 [ {rank} ] but they are close to meeting the auto-pass threshold")
-            else:
-                st.error(f"AUTO-PASS FAIL: The player has only played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is not above the {rankTarget*100}%  threshold for a team ranked between 21–30 [ {rank} ]")
-        
-        elif rank <=50:
-            rankTarget = .70
-            if pct_played >= rankTarget:
-                st.success(f"AUTO-PASS GRANTED: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months for a team ranked between 31–50 [ {rank} ]")
-            elif pct_played >= .20:
-                st.warning(f"AUTO-PASS NEAR-MISS: The player has played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is just below the {rankTarget*100}% threshold for a team ranked between 31–50 [ {rank} ] but they are close to meeting the auto-pass threshold")
-            else:
-                st.error(f"AUTO-PASS FAIL: The player has only played {int(pct_played * 100)}% of competitive matches in the last 24 months, this is not above the {rankTarget*100}%  threshold for a team ranked between 31–50 [ {rank} ]")
-
-        
-        elif rank >50:
-            st.error(f"AUTO-PASS FAIL: {final_teams[0]}'s FIFA Raking [{rank}] is below the auto-pass threshold of 50. Therefore the auto-pass criteria has not been met")
-
-        else:
-            st.warning(f"RANK UNDER CONSTRUCTION")
 
     else:
         st.error(f"AUTO-PASS FAIL: The player has not played for their {final_teams[0]}'s national team. Therefore the auto-pass criteria has not been met")
